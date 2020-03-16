d071031f090ea98737edf13aabf99ba689554dbe
TatsukichiShibuya <54014820+TatsukichiShibuya@users.noreply.github.com>
2020-03-16 19:52:22 +0900

    Update BellmanFord.java

--- SourceCodes/BellmanFord.java
+++ SourceCodes/BellmanFord.java
@@ -1,6 +1,7 @@
 public static class BellmanFord{
     private ArrayList<Edge> edge;
     long[] dist; int v, e;/*v:頂点数,e:辺数*/
+    boolean[] nega;
     boolean directed;
     public BellmanFord(int v, int e, boolean directed){
         this.directed = directed;
@@ -8,26 +9,27 @@ public static class BellmanFord{
         this.e = this.directed?e:2*e;
         edge = new ArrayList<>(this.e);
         dist = new long[v];
+        nega = new boolean[v];
         Arrays.fill(dist,INF);
+        Arrays.fill(nega, false);
     }
-    public boolean bellmanford(int s){//if a negative cycle exists, return true
+    public void bellmanford(int s){
         dist[s] = 0;
         boolean update;
-        for (int j = 0; j < v; j++) {
+        for (int j = 0; j < 2*v-1; j++) {
             update = false;
             for (int i = 0; i < edge.size(); i++) {
                 Edge e = edge.get(i);
                 if(dist[e.from]!=INF && dist[e.to]>dist[e.from]+e.cost){
                     dist[e.to] = dist[e.from]+e.cost;
                     update = true;
-                    if(j == v-1){
-                        return true;
+                    if(j >= v-1){
+                        nega[e.to] = true;
                     }
                 }
             }
             if(!update) break;
         }
-        return false;
     }
     public void addEdge(int from, int to, long cost){
         Edge e = new Edge(from, to, cost);
